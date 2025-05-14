acaed72f6f5c64f2a18e6d7892902aacc46880cb
garuleon93 <patricio.leonb93@gmail.com>
2025-05-14 15:32:13 -0500

    12345555

--- aplicacion/vista/vista_principal.py
+++ aplicacion/vista/vista_principal.py
@@ -3,12 +3,14 @@ from vista import vista_contratistas
 from vista import vista_facturas
 from vista import vista_informes
 
+<<<<<<< Updated upstream
+=======
+#hasta aqui
+>>>>>>> Stashed changes
 
 class Vista_Principal:
     def __init__(self, page):
-        self.page = page  # Aquí agregamos la página
-           
-
+        self.page = page 
     
     def crear_pantalla_principal(self):
 
@@ -31,7 +33,7 @@ class Vista_Principal:
             animation_duration=0,
             selected_index=0,
             expand=1,
-            label_color=ft.colors.BLUE_800,
+            label_color=ft.colors.BLACK,
             unselected_label_color=ft.colors.BLUE_200,
             tabs=[tab1, tab2,tab3]
         )
@@ -41,34 +43,3 @@ class Vista_Principal:
         
         self.page.add(titulo,tabs)
 
-    
-    """def crear_pantalla_principal(self):
-
-
-        def pantalla_contratistas():
-                return vista_contratistas.obtener_pantalla_contratistas()
-
-        def pantalla_facturas():
-                return vista_facturas.obtener_pantalla_facturas()
-        
-        def pantalla_informes():
-                return vista_informes.obtener_pantalla_informes()
-       
-        #TABS PARA  CONTRATISTAS - FACTURAS - GENENRAR NFORMES
-        tabs=ft.Tabs(
-            selected_index=0,
-            animation_duration=50,
-            tabs=[
-                ft.Tab(text="Contratistas",icon=ft.icons.PERSON,content=pantalla_contratistas()),
-                ft.Tab(text="Facturas",icon=ft.icons.DOCUMENT_SCANNER,content=pantalla_facturas()),
-                ft.Tab(text="Generar Informes",icon=ft.icons.SCHEDULE,content=pantalla_informes()),
-            ],
-            expand=1,
-            label_color=ft.colors.BLUE_800,
-            unselected_label_color=ft.colors.BLUE_200
-            
-        )
-        titulo=ft.Text(value="Gestón de Contratistas",size=24,color=ft.colors.BLUE_800)
-        
-        self.page.add(titulo,tabs)
-        """
